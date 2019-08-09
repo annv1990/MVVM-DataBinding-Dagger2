ecb8fac16df23e24df54f8cba7bdd84cfc36aadc
AnNV <an.nguyen@co-well.vn>
2019-08-09 17:51:59 +0700

    -init

---
+++ app/src/main/java/com/ajs/simplemvvm/MainActivity.java
@@ -0,0 +1,40 @@
+package com.ajs.simplemvvm;
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    HeroesAdapter adapter;
+
+    List<Hero> heroList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        HeroesViewModel model = ViewModelProviders.of(this).get(HeroesViewModel.class);
+
+        model.getHeroes().observe(this, new Observer<List<Hero>>() {
+            @Override
+            public void onChanged(@Nullable List<Hero> heroes) {
+                adapter = new HeroesAdapter(MainActivity.this, heroes);
+                recyclerView.setAdapter(adapter);
+            }
+        });
+    }
+}
